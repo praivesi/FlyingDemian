eb11adaaf57f61e0de63d0b4225612e38bb1266b
hsoh <hsoh@ivstech.co.kr>
2021-11-30 18:47:43 +0900

    Initial commit - add SpringBoot Initializr

---
+++ src/test/java/com/flying/demian/DemianApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flying.demian;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemianApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
